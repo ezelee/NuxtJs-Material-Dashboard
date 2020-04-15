7ab01d28510cc0ed040d19ff7ce9c00c8795df57
Lee Ezequiel Ariel <elee@sancorseguros.com>
2020-04-15 14:59:04 -0300

    Se agrega servicio de empleados. Se agregan datos en la db.json

---
+++ services/EmployeeService.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: `http://localhost:3213`,
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getHeaders() {
+    return apiClient.get('/headers')
+  },
+  getItems() {
+    return apiClient.get('/items')
+  },
+  apiDailySalesChart() {
+    return apiClient.get('/dailySalesChart')
+  },
+  apiDataCompletedTasksChart() {
+    return apiClient.get('/dataCompletedTasksChart')
+  },
+  apiEmailsSubscriptionChart() {
+    return apiClient.get('/emailsSubscriptionChart')
+  }
+}
