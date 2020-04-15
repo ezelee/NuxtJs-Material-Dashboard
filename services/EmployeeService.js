9608a4b362acebf4548f24d1b450377a42b35d2f
Lee Ezequiel Ariel <elee@sancorseguros.com>
2020-04-15 15:10:15 -0300

    Se corrige url del servicio de empleados

--- services/EmployeeService.js
+++ services/EmployeeService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3213`,
+  baseURL: `http://my-json-server.typicode.com/ezelee/NuxtJs-Material-Dashboard`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
